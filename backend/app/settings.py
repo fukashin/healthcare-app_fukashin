99d497dc55d747b55e3a603c4f4b7b9d1ad2df08
fukashin <55280398+fukashin@users.noreply.github.com>
2024-12-09 21:28:18 +0900

    fix docker修正 setting.py日本に

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'basicapi',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
